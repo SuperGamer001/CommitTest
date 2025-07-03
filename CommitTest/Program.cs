c61f55ea65ca7a4bb2be2654d317afabf96ede11
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-07-03 13:39:20 -0700

    Made a test method

--- CommitTest/Program.cs
+++ CommitTest/Program.cs
@@ -2,3 +2,14 @@
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hey there Mr. Blue!");
+
+TestMethod();
+
+
+
+
+void TestMethod()
+{
+    // This is a test method
+    Console.WriteLine("This is a test method.");
+}
